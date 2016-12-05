789c9c456a45f9dbe9eb4b0c91198baa34f761ce
iqingchen <1367524009@qq.com>
2016-12-05 10:06:31 +0800

    主页图片显示

--- DY/DY/Classes/Main/View/CollectionNormalCell.swift
+++ DY/DY/Classes/Main/View/CollectionNormalCell.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+import Kingfisher
 
 class CollectionNormalCell: UICollectionViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var onlineBtn: UIButton!
+    @IBOutlet weak var nickNameLabel: UILabel!
+    @IBOutlet weak var roomNameLabel: UILabel!
+
+    var anchor : AnchorModel? {
+        didSet {
+            // 0.校验模型是否有值
+            guard let anchor = anchor else {return}
+            // 1.在线人数显示文字
+            var onlineStr : String = ""
+            if anchor.online >= 10000 {
+                onlineStr = "\(Int(anchor.online / 10000))万在线"
+            } else {
+                onlineStr = "\(anchor.online)在线"
+            }
+            onlineBtn.setTitle(onlineStr, for: UIControlState())
+            // 2.昵称的显示
+            nickNameLabel.text = anchor.nickname
+            roomNameLabel.text = anchor.room_name
+            // 3.设置封面图片
+            guard let iconUrl = URL(string: anchor.vertical_src) else {return}
+            iconImageView.kf.setImage(with: iconUrl)
+        }
     }
 
 }
